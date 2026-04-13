903d9de37ce99e3ff1ab9352ac848ed06c622857
PRABHU PRASAD LENKA <prabhuprasadlenka@outlook.com>
2026-04-13 18:20:32 +0530

    initial portfolio

---
+++ js/main.js
@@ -0,0 +1,189 @@
+; (function () {
+
+	const roles = [
+		"Full Stack Developer",
+		".NET Developer",
+		"React Developer",
+		"UI Developer"
+	];
+
+	let roleIndex = 0;
+	let charIndex = 0;
+	const typingSpeed = 80;
+	const eraseSpeed = 40;
+	const delayBetweenRoles = 1500;
+
+	const typingElement = document.getElementById("typing-text");
+
+	function type() {
+		if (charIndex < roles[roleIndex].length) {
+			typingElement.textContent += roles[roleIndex].charAt(charIndex);
+			charIndex++;
+			setTimeout(type, typingSpeed);
+		} else {
+			setTimeout(erase, delayBetweenRoles);
+		}
+	}
+
+	function erase() {
+		if (charIndex > 0) {
+			typingElement.textContent = roles[roleIndex].substring(0, charIndex - 1);
+			charIndex--;
+			setTimeout(erase, eraseSpeed);
+		} else {
+			roleIndex = (roleIndex + 1) % roles.length;
+			setTimeout(type, typingSpeed);
+		}
+	}
+
+	document.addEventListener("DOMContentLoaded", function () {
+		type();
+	});
+
+	'use strict';
+
+	var isMobile = {
+		Android: function () {
+			return navigator.userAgent.match(/Android/i);
+		},
+		BlackBerry: function () {
+			return navigator.userAgent.match(/BlackBerry/i);
+		},
+		iOS: function () {
+			return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+		},
+		Opera: function () {
+			return navigator.userAgent.match(/Opera Mini/i);
+		},
+		Windows: function () {
+			return navigator.userAgent.match(/IEMobile/i);
+		},
+		any: function () {
+			return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+		}
+	};
+
+
+	var fullHeight = function () {
+
+		if (!isMobile.any()) {
+			$('.js-fullheight').css('height', $(window).height());
+			$(window).resize(function () {
+				$('.js-fullheight').css('height', $(window).height());
+			});
+		}
+	};
+
+	// Parallax
+	var parallax = function () {
+		$(window).stellar();
+	};
+
+	var contentWayPoint = function () {
+		var i = 0;
+		$('.animate-box').waypoint(function (direction) {
+
+			if (direction === 'down' && !$(this.element).hasClass('animated-fast')) {
+
+				i++;
+
+				$(this.element).addClass('item-animate');
+				setTimeout(function () {
+
+					$('body .animate-box.item-animate').each(function (k) {
+						var el = $(this);
+						setTimeout(function () {
+							var effect = el.data('animate-effect');
+							if (effect === 'fadeIn') {
+								el.addClass('fadeIn animated-fast');
+							} else if (effect === 'fadeInLeft') {
+								el.addClass('fadeInLeft animated-fast');
+							} else if (effect === 'fadeInRight') {
+								el.addClass('fadeInRight animated-fast');
+							} else {
+								el.addClass('fadeInUp animated-fast');
+							}
+
+							el.removeClass('item-animate');
+						}, k * 100, 'easeInOutExpo');
+					});
+
+				}, 50);
+
+			}
+
+		}, { offset: '85%' });
+	};
+
+
+
+	var goToTop = function () {
+
+		$('.js-gotop').on('click', function (event) {
+
+			event.preventDefault();
+
+			$('html, body').animate({
+				scrollTop: $('html').offset().top
+			}, 500, 'easeInOutExpo');
+
+			return false;
+		});
+
+		$(window).scroll(function () {
+
+			var $win = $(window);
+			if ($win.scrollTop() > 200) {
+				$('.js-top').addClass('active');
+			} else {
+				$('.js-top').removeClass('active');
+			}
+
+		});
+
+	};
+
+	var pieChart = function () {
+		$('.chart').easyPieChart({
+			scaleColor: false,
+			lineWidth: 4,
+			lineCap: 'butt',
+			barColor: '#FF9000',
+			trackColor: "#f5f5f5",
+			size: 160,
+			animate: 1000
+		});
+	};
+
+	var skillsWayPoint = function () {
+		if ($('#fh5co-skills').length > 0) {
+			$('#fh5co-skills').waypoint(function (direction) {
+
+				if (direction === 'down' && !$(this.element).hasClass('animated')) {
+					setTimeout(pieChart, 400);
+					$(this.element).addClass('animated');
+				}
+			}, { offset: '90%' });
+		}
+
+	};
+
+
+	// Loading page
+	var loaderPage = function () {
+		$(".fh5co-loader").fadeOut("slow");
+	};
+
+
+	$(function () {
+		contentWayPoint();
+		goToTop();
+		loaderPage();
+		fullHeight();
+		parallax();
+		// pieChart();
+		skillsWayPoint();
+	});
+
+
+}());
